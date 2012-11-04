b96ed9f83fd5494bca6dfa5d644cf2ffc24cf5cb
elff1 <elff1@21cn.com>
2012-11-04 20:42:43 +0800

    Initialization, using taobao source, rails 3.2.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Depot::Application.config.secret_token = 'aa7b1b4ae7fbba73ef6e7439b82077493f7bc1965c8d7cccac1df80620520d28ccde7c637d9b9911a3782aa6f41eb347bad0fa884bc7ab509b96cf90980b0139'
