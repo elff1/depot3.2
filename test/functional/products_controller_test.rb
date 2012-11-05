d8e76e2b0751084ad7fc42ffaa855aaa26c65039
elff1 <elff1@21cn.com>
2012-11-06 01:44:03 +0800

    A2: Making Prettier Listings; switch to Rails 3.2.6

--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -1,3 +1,11 @@
+#---
+# Excerpted from "Agile Web Development with Rails",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material, 
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose. 
+# Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
+#---
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
@@ -18,7 +26,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should create product" do
     assert_difference('Product.count') do
-      post :create, product: @product.attributes
+      post :create, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     end
 
     assert_redirected_to product_path(assigns(:product))
@@ -35,7 +43,7 @@ class ProductsControllerTest < ActionController::TestCase
   end
 
   test "should update product" do
-    put :update, id: @product, product: @product.attributes
+    put :update, id: @product, product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title }
     assert_redirected_to product_path(assigns(:product))
   end
 
